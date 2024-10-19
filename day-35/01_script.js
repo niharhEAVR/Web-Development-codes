0f4d1c9ab740f446b2b1f51d74586b91281fe049
niharhEAVR <debnathnihar14@gmail.com>
2024-10-19 12:47:30 +0530

    commit

---
+++ day-35/01_script.js
@@ -0,0 +1,12 @@
+const myName  = "Nihar Debnath"
+let age = 19
+var profession = "student"
+
+address = "Shantipur"
+// in javascript we can also declare variable without the let, var or const keyword, but this method is not reccomandable
+
+console.table([myName, age, profession,address])
+
+console.log(typeof address)
+
+// console.table is used to print all the variables togather and in table form
